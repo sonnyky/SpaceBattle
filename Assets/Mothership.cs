6c4dde25e37c0d5cc09958accc22818d32914e3b
Kyle Troutner <kyletroutner1@gmail.com>
2016-02-01 20:34:33 +0900

    Add script to create a sphere when button is pressed

---
+++ Assets/Mothership.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mothership : MonoBehaviour {
+
+	public Object sphere;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void createSphere() {
+		Instantiate (sphere, new Vector3 (0, -2, 0), Quaternion.identity);
+	}
+}
