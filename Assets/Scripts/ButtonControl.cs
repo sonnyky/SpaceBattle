16db93fc4aed861a9dd85326089997c10eb92219
Kyle Troutner <kyletroutner1@gmail.com>
2016-02-11 23:44:53 +0900

    Set a timer on the button to control when player can launch a jet (TODO: Currently causes a lot of null references, even though behavior is correct.  Need to fix...)
    Make behavior of all buttons the same, and remove placeholder sphere resources

---
+++ Assets/Scripts/ButtonControl.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ButtonControl : MonoBehaviour {
+
+	/* The base ship for this button.
+	 * When the button is pressed, a ship will be launched
+	 * from this mothership.
+	 * (Typically this should be the player mothership)
+	*/
+	public Mothership hq;
+	// The length of time to disable the button after a click
+	public float interval;
+
+	private float timer;
+
+
+	// Use this for initialization
+	void Start () {
+		timer = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer -= Time.deltaTime;
+
+		if (gameObject.GetComponent<Button> ().interactable == false && timer <= 0) {
+			// reset timer to zero in case it went negative
+			timer = 0;
+			gameObject.GetComponent<Button> ().interactable = true;
+		}
+	}
+
+	public void launch() {
+		hq.createFighterJets ();
+		gameObject.GetComponent<Button> ().interactable = false;
+		this.timer = interval;
+	}
+
+}
