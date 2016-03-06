f2f32e84d337deb0ad4b22e0ac99b958515e5848
sonnyky <sonny_yap@hotmail.com>
2016-03-06 18:50:53 +0900

    Add enemy fighter, hit point block, and they take damage from the player

---
+++ Assets/Scripts/HitPointBlock.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitPointBlock : MonoBehaviour {
+    private Vector3 curScale;
+	// Use this for initialization
+	void Start () {
+        curScale = gameObject.transform.localScale;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void HitPointReduceTo(float scale)
+    {
+        curScale.y = scale * curScale.y;
+        gameObject.transform.localScale = curScale;
+    }
+}
