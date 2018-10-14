6d22e0ed6ea5d0574799e32298157939c9af98ab
Sonny Kurniawan <sonny.kurniawan.yap@gmail.com>
2018-10-14 23:26:26 +0900

    upgrade unity

--- Assets/Scripts/HitPointBlock.cs
+++ Assets/Scripts/HitPointBlock.cs
@@ -3,8 +3,12 @@ using System.Collections;
 
 public class HitPointBlock : MonoBehaviour {
     private Vector3 curScale;
+
+    Renderer barRenderer;
+
 	// Use this for initialization
 	void Start () {
+        barRenderer = GetComponent<Renderer>();
         curScale = gameObject.transform.localScale;
     }
 	
@@ -15,7 +19,19 @@ public class HitPointBlock : MonoBehaviour {
 
     public void HitPointReduceTo(float scale)
     {
+        float originalValue = barRenderer.bounds.min.y;
+        Debug.Log("original value : " + originalValue);
         curScale.y = scale * curScale.y;
         gameObject.transform.localScale = curScale;
+
+        float newValue = barRenderer.bounds.min.y;
+        Debug.Log("new value : " + newValue);
+
+        //calculate difference
+        float difference = newValue - originalValue;
+
+        //move the bar to the left
+        transform.Translate(new Vector3(-0f, -difference, 0f));
+
     }
 }
